88823f7473a7cafa5f4bba8e640cf039cbe57aef
Alan Almeida <alanalmeida.emp@gmail.com>
2022-11-30 19:26:43 -0300

    refatorando a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -0,0 +1,12 @@
+import { CategoriesRepository } from '../../repositories/CategoriesRepository';
+import { ListCategoriesController } from './ListCategoryController';
+import { ListCategoriesUseCase } from './ListCategoryUseCase';
+
+const categoriesRepository = new CategoriesRepository();
+
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+const listCategoriesController = new ListCategoriesController(
+  listCategoriesUseCase,
+);
+
+export { listCategoriesController };
