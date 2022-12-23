55ff7c99d4a0450724f49e86fcbc66c2e4bc2dcd
Alan Almeida <alanalmeida.emp@gmail.com>
2022-12-23 17:14:26 -0300

    Alterando o repositorio da categoria (aula 8 - mod 2 - chapter 3)

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,3 +1,5 @@
+import { getRepository, Repository } from 'typeorm';
+
 import { Category } from '../../entities/Category';
 import {
   ICategoriesRepository,
@@ -5,12 +7,11 @@ import {
 } from '../ICategoriesRepository';
 
 class CategoriesRepository implements ICategoriesRepository {
-  private categories: Category[];
-
+  private repository: Repository<Category>; // É a instancia do Repository do typeorm que dara acesso a todos os metodos do ORM, permitindo assim alterar os dados no banco. Esse metodos serão utilizados para manipular os dados
   private static INSTANCE: CategoriesRepository; // Atributo de classe
 
   private constructor() {
-    this.categories = [];
+    this.repository = getRepository(Category); // Vai gerar o repository do typeORM utilizando a Entity Category
   }
 
   public static getInstance(): CategoriesRepository {
@@ -22,24 +23,23 @@ class CategoriesRepository implements ICategoriesRepository {
     return CategoriesRepository.INSTANCE;
   }
 
-  create({ name, description }: ICreateCategoryDTO): void {
-    const category = new Category();
-
-    Object.assign(category, {
-      name,
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({
+      // Cria uma nova entidade de fato, utilizando a Entity Category como base
       description,
-      created_at: new Date(),
+      name,
     });
 
-    this.categories.push(category);
+    await this.repository.save(category); // Salvando o item na tabela de fato
   }
 
-  list(): Category[] {
-    return this.categories;
+  async list(): Promise<Category[]> {
+    const categories = await this.repository.find(); // Utilizando o método find() do repository para buscar todos as categories salvaas no banco
+    return categories;
   }
 
-  findByName(name: string): Category {
-    const category = this.categories.find(category => category.name === name);
+  async findByName(name: string): Promise<Category> {
+    const category = await this.repository.findOne({ name });
 
     return category;
   }
