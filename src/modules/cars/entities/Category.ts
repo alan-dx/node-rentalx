4be45dafb46e7a8efcd5b86e80949be23cc10e53
Alan Almeida <alanalmeida.emp@gmail.com>
2022-12-23 16:36:38 -0300

    Refatorando model de Categoria (aula 7 - mod 2 - chapter 3)

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('categories') // Declarando que essa entity irá representar os dados presentes na Tabela categories
+class Category {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Category };
