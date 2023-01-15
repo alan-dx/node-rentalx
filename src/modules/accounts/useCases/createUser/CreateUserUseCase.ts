cbc2a6f625993e3cb647f3f0a6f0f9d0b364e951
Alan Almeida <alanalmeida.emp@gmail.com>
2023-01-15 20:10:38 -0300

    Criptografar senha (aula 5 - mod 4 - chapter 3)

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
@@ -17,12 +18,20 @@ class CreateUserUseCase {
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
     this.usersRepository.create({
       name,
       // username,
       email,
       driver_license,
-      password,
+      password: passwordHash,
     });
   }
 }
