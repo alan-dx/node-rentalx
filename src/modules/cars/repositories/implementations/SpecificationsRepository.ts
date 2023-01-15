c3ab7d7f8938f2541da7b4806b7aa689c439cf8b
Alan Almeida <alanalmeida.emp@gmail.com>
2023-01-15 17:59:52 -0300

    Criando migration de especificacao (aula 3 - mod 3 - chapter 3)

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { Repository, getRepository } from 'typeorm';
+
 import { Specification } from '../../entities/Specification';
 import {
   ISpecificationRepository,
@@ -5,28 +7,25 @@ import {
 } from '../ISpecificationRepository';
 
 class SpecificationRepository implements ISpecificationRepository {
-  specifications: Specification[];
+  private repository: Repository<Specification>;
 
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
 
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-
-    Object.assign(specification, {
-      name,
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
       description,
-      created_at: new Date(),
+      name,
     });
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
 
-  findByName(name: string): Specification {
-    const specification = this.specifications.find(
-      specification => specification.name === name,
-    );
+  async findByName(name: string): Promise<Specification> {
+    const specification = await this.repository.findOne({
+      name,
+    });
 
     return specification;
   }
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -13,8 +13,8 @@ class CreateSpecificationUseCase {
     private specificationRepository: ISpecificationRepository,
   ) {}
 
-  execute({ name, description }: IRequest): void {
-    const specificationAlreadyExists = this.specificationRepository.findByName(
+  async execute({ name, description }: IRequest): Promise<void> {
+    const specificationAlreadyExists = await this.specificationRepository.findByName(
       name,
     );
 
@@ -22,7 +22,7 @@ class CreateSpecificationUseCase {
       throw new Error('Specifications already exists!');
     }
 
-    this.specificationRepository.create({ name, description });
+    await this.specificationRepository.create({ name, description });
   }
 }
 
