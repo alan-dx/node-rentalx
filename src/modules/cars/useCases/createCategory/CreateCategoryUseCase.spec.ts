c8d13862726977b5a38fb04c2b4ff66423f7c51d
Alan Almeida <alanalmeida.emp@gmail.com>
2023-01-28 15:05:28 -0300

    Teste de criacao de categoria (aula 4 - mod 1 - chapter 4)

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,7 +1,69 @@
-describe('Criar categoria', () => {
-  it('Espere que 2 + 2, seja 4', () => {
-    const soma = 2 + 2;
-    const resultado = 4;
-    expect(soma).toBe(resultado);
+import { AppError } from '../../../../errors/AppError';
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    );
+  });
+
+  test('should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Test',
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name,
+    );
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  test('should not be able to create a category that already exists', async () => {
+    // expect(async () => {
+    //   const category = {
+    //     name: 'Category Test',
+    //     description: 'Category description Test',
+    //   };
+
+    //   await createCategoryUseCase.execute({
+    //     name: category.name,
+    //     description: category.description,
+    //   });
+
+    //   await createCategoryUseCase.execute({
+    //     name: category.name,
+    //     description: category.description,
+    //   });
+    // }).rejects.toBeInstanceOf(AppError);
+
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Test',
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const promise = createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    expect(promise).rejects.toBeInstanceOf(AppError);
   });
 });
