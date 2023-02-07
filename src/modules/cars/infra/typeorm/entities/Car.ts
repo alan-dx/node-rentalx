203a1ff1cc48c012f916b75ddfb00ea0870d9602
Alan Almeida <alanalmeida.emp@gmail.com>
2023-02-07 00:06:42 -0300

    Estruturando a entidade de carros (aula 5 - mod 3 - chapter 4)

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,22 +1,55 @@
+import {
+  Column,
+  Entity,
+  PrimaryColumn,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+
+@Entity('cars')
 class Car {
+  @PrimaryColumn() // Pk
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   daily_rate: number;
+
+  @Column()
   available: boolean;
+
+  @Column()
   license_plate: string;
+
+  @Column()
   fine_amount: number;
+
+  @Column()
   brand: string;
+
+  @ManyToOne(() => Category) // Indicando o tipo de relacionamento
+  @JoinColumn({ name: 'category_id' }) // Indicando qual coluna dessa tabela eh a FK
+  category: Category; // Referenciando a Entity target (como a Entity eh a representante da tabela, a partir dela determina-se a tabela alvo)
+
+  @Column()
   category_id: string;
+
+  @CreateDateColumn() // Seta pra data atual por default
   created_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
       this.available = true;
-      this.created_at = new Date();
     }
   }
 }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from 'tsyringe';
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationRepository';
 
@@ -24,3 +26,6 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
+
+// ICarsRepository
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
