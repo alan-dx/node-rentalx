a061c0d5a8335dfcee925caca98afee6e3f47a17
Alan Almeida <alanalmeida.emp@gmail.com>
2023-03-11 19:51:59 -0300

    Configurando supertest (aula 1 - mod 5 - chapter 4)

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import '@shared/container';
+
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
+import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
+
+import { router } from './routes';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+    next(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,3 @@
-import 'reflect-metadata';
-import '@shared/container';
-
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
-
-import { router } from './routes';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use(router);
-
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-    next(err);
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`,
-    });
-  },
-);
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server started'));
