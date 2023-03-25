07c2f6c8ba39aed0e4cf2f2b3edbf264fe9db6dd
Alan Almeida <alanalmeida.emp@gmail.com>
2023-03-24 23:24:11 -0300

    Controller refresh token (aula 5 - mod 2 - chapter 5)

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -1,5 +1,5 @@
 import { sign, verify } from 'jsonwebtoken';
-import { inject } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 
 import auth from '@config/auth';
 import { IUserTokensRepository } from '@modules/accounts/repositories/IUserTokensRepository';
@@ -11,6 +11,7 @@ interface IPayload {
   email: string;
 }
 
+@injectable()
 class RefreshTokenUseCase {
   constructor(
     @inject('UsersTokensRepository')
--- src/shared/infra/http/middlewares/ensureAuthentication.ts
+++ src/shared/infra/http/middlewares/ensureAuthentication.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import auth from '@config/auth';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -14,6 +16,7 @@ export async function ensureAuthentication(
   next: NextFunction,
 ): Promise<void> {
   const authHeader = request.headers.authorization;
+  const userTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError('Token missing', 401);
@@ -24,11 +27,13 @@ export async function ensureAuthentication(
   try {
     const { sub: user_id } = verify(
       token,
-      '484f1c5d540e55294143e3d476346509',
+      auth.secret_refresh_token,
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
+    const user = await userTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token,
+    );
 
     if (!user) {
       throw new AppError('User does not exists!', 401);
